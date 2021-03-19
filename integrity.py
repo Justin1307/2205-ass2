498ad45f0f74e687064ef214decd4bd2c6cc7cb8
modeevandee <60996464+modeevandee@users.noreply.github.com>
2021-03-19 20:24:04 +0800

    integrity part

--- integrity.py
+++ integrity.py
@@ -1,1 +1,28 @@
-import integrity
+from hashlib import sha256
+import pyotp
+import time
+
+
+# client and server needs to have their own copy of the secret
+# base32secret = pyotp.random_base32()
+# alternative specify own secret
+base32secret = 'S3K3TPI5MYA2M67V'
+string_integrity = 'I am a string that needs integrity'
+# instanciate totp
+totp = pyotp.TOTP(base32secret)
+# combine string and otp for hashing
+to_integrity = string_integrity + totp.now()
+
+
+
+# test the hash
+print(sha256(to_integrity.encode('utf-8')).hexdigest())
+
+
+#verification process
+otp_val = totp.now()
+# concatenate the decrypted string msg with otp value
+msg_received = string_integrity + otp_val
+# then sha256 it
+print(sha256(msg_received.encode('utf-8')).hexdigest())
+
