b527a01a5133ceb9249f2e7285eedcb38891bfad
Justin1307 <limkeathui_justin99@yahoo.com.sg>
2021-03-21 02:33:50 +0800

    Editted the code

--- integrity.py
+++ integrity.py
@@ -4,28 +4,41 @@ import pyotp
 delimiter = "******"
 
 # client-side
-data = 'I am a string that needs integrity'
 
-# Generate the OTP and sha256 of the data
-OTP = int(pyotp.random_hex(),16)
-sha256_result = int(sha256(data.encode('utf-8')).hexdigest(),16)
+def generateIntegrity(data):
 
-# XOR the OTP and the sha256_result, append the delimiter and the OTP to the back 
-integrityResult = str(sha256_result ^ OTP)
-data_msg = integrityResult + delimiter + str(OTP)
+    # Generate the OTP and sha256 of the data
+    OTP = int(pyotp.random_hex(),16)
+    sha256_result = int(sha256(data.encode('utf-8')).hexdigest(),16)
+
+    # XOR the OTP and the sha256_result, append the delimiter and the OTP to the back 
+    integrityResult = str(sha256_result ^ OTP)
+    data_msg = integrityResult + delimiter + str(OTP)
+    return data_msg
 
 # Server-side
 
 #verification process
 
-# Split the data according to the delimiter
-dataSplit = data_msg.split(delimiter)
-integrityResult = int(dataSplit[0])
-OTP = int(dataSplit[1])
+def verifyIntegrity(data,msg):
 
-# XOR the result to get back the hash
-to_be_verified = integrityResult ^ OTP
+    # Split the data according to the delimiter
+    dataSplit = data.split(delimiter)
+    integrityResult = int(dataSplit[0])
+    OTP = int(dataSplit[1])
 
-# Verify
-print(to_be_verified==sha256_result)
+    # Generate the hash of the data
+    sha256_result = int(sha256(msg.encode('utf-8')).hexdigest(),16)
 
+    # XOR the result to get back the hash
+    to_be_verified = integrityResult ^ OTP
+
+    # Verify
+    checkResult = to_be_verified == sha256_result
+
+    return checkResult
+
+data = "Hi i am justin"
+data_msg = generateIntegrity(data)
+check = verifyIntegrity(data_msg,data)
+print (check)
